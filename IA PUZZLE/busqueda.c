2f6b8c1f2ad33253224cff0b7fa28ee082e88a03
Javier Herrera <62066426+JavierHerreraaa@users.noreply.github.com>
2020-11-14 12:12:08 +0100

    Add files via upload

---
+++ IA PUZZLE/busqueda.c
@@ -0,0 +1,179 @@
+/*******************************************/
+/* 		    BUSQUEDA.C                     */
+/*						                   */
+/* Asignatura: Inteligencia Artificial     */
+/* Grado en Ingenieria Informatica - UCA   */
+/*******************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "puzle.h"
+#include "listaia.h"
+#include "busqueda.h"
+
+void solucionFin(int res){
+   printf("\nFin de la busqueda\n");
+
+    if (res)
+      printf("Se ha llegado al objetivo\n");
+   else
+      printf("No se ha llegado al objetivo\n");
+}
+void dispNodo(tNodo *nodo){
+    dispEstado(nodo->estado);
+    printf("\n");
+}
+void dispCamino(tNodo *nodo){
+    if (nodo->padre==NULL){
+        printf("\n Desde el inicio\n");
+        dispEstado(nodo->estado);
+    }
+    else {
+        dispCamino(nodo->padre);
+         dispOperador(nodo->operador);
+        dispEstado(nodo->estado);
+    }
+}
+
+void dispSolucion(tNodo *nodo){
+   printf("Profundidad=%d\n",nodo->profundidad);
+   printf("Coste=%d\n",nodo->costeCamino);
+   dispCamino(nodo);
+}
+
+
+/* Crea el nodo raíz. */
+tNodo *nodoInicial(){
+   tNodo *inicial=(tNodo *) malloc(sizeof(tNodo));
+   inicial->estado=estadoInicial();
+   inicial->padre=NULL;
+   inicial->costeCamino=0;
+   inicial->profundidad=0;
+   return inicial;
+}
+
+LISTA expandir(tNodo *nodo){
+    unsigned op;
+    LISTA sucesores=VACIA;
+    tNodo *nuevo=calloc(1,sizeof(tNodo));
+    tEstado *s;
+    for (op=1; op<=NUM_OPERADORES;op++){
+      if (esValido(op,nodo->estado)){
+                        //s=(tEstado *)calloc(1,sizeof(tEstado));
+          s=aplicaOperador(op,nodo->estado);
+          nuevo->estado=s;
+          nuevo->padre=nodo;
+          nuevo->operador=op;
+          nuevo->costeCamino=nodo->costeCamino + coste(op,nodo->estado);
+          nuevo->profundidad=nodo->profundidad+1;
+          InsertarUltimo(&sucesores,  (tNodo *) nuevo, (sizeof (tNodo)));
+      }
+  }
+return sucesores;
+}
+
+int busqueda(){
+    int objetivo=0, visitados=0, p = 0;
+    tNodo *Actual=(tNodo*) calloc(1,sizeof(tNodo));
+    tNodo *Inicial=nodoInicial();
+
+    LISTA Abiertos= VACIA;                                  //Se guardan los nodos abiertos (pendientes a tratar)
+    LISTA Sucesores= VACIA;                                 //Se guardan los nodos a expandir
+    LISTA Cerrados = VACIA;                                 //Se guardan los nodos que ya han sido tratados
+    InsertarPrimero(&Abiertos,(tNodo*) Inicial,sizeof(tNodo));
+    while (!esVacia(Abiertos) && !objetivo){
+
+        Actual=(tNodo*) calloc(1,sizeof(tNodo));
+        ExtraerPrimero(Abiertos,Actual, sizeof(tNodo));
+        EliminarPrimero(&Abiertos);
+        p =buscar_repetidos(Actual,Cerrados);
+        if(p==0){
+            objetivo=testObjetivo(Actual->estado);
+            if (!objetivo){
+                visitados++;
+                Sucesores = expandir(Actual);
+                InsertarPrimero(&Cerrados,(tNodo*) Actual, sizeof(tNodo));     //Almalceno el contenido de Abiertos en Cerrado
+                imprimir(Cerrados);
+                Abiertos=  Concatenar(Abiertos,Sucesores);        //Cambiando los parametros se convierten en ANCHURA o PROFUNDIDAD
+
+                }
+            }
+        }//while
+    //Iteracion lista
+
+    printf("\nVisitados= %d\n", visitados);
+    if (objetivo)
+        dispSolucion(Actual);
+    free(Sucesores);
+    free(Inicial);
+    free(Actual);
+    return objetivo;
+}
+
+int buscar_repetidos(tNodo *Actual,  LISTA Cerrados){      // Pasamos la lista de Sucesores y el nodo actual
+
+    int igual = 0;
+    tNodo *Sucesor =(tNodo*) calloc(1,sizeof(tNodo));
+
+   if(!esVacia(Cerrados)){
+        LISTA Auxiliar = Cerrados;                            //Comparamos todos los nodos de la lista de Sucesores con el nodo actual
+
+        while(!esVacia(Auxiliar) && igual == 0){
+
+            ExtraerPrimero(Auxiliar,Sucesor,sizeof(tNodo));
+
+            igual = iguales(Actual->estado, Sucesor->estado);
+
+            Auxiliar = Auxiliar->next;
+
+            }
+   }
+
+    return igual;
+}
+
+int profundidad_limitada(int limite, tNodo *Actual){
+
+    return(limite > Actual->profundidad);
+}
+
+int funcion_heuristica(tNodo *Actual){         //Vamos a calcular el numero de piezas mal colocadas (filas y columnas)
+    tNodo *Final =(tNodo*) calloc(1,sizeof(tNodo));
+    Final->estado = estadoObjetivo();
+    unsigned int fila1 ,columna1, fila2, columna2, total=0 ;
+
+    for(fila1=0;fila1<N;fila1++){
+        for(columna1=0;columna1<N;columna1++){
+            for(fila2 = 0; fila2<N; fila2++){
+                for(columna2 = 0; columna2<N; columna2++){
+                    if(Actual->estado->celdas[fila1][columna1] == Final->estado->celdas[fila2][columna2]){
+                        total = abs(fila1-fila2 + columna1 - columna2) + total;
+                    }
+
+                }
+            }
+        }
+    }
+    Actual->valHeuristica = total;
+    return total;
+
+}
+
+void imprimir(LISTA Cerrados){
+
+LISTA Auxiliar = Cerrados;
+tNodo *Sucesor =(tNodo*) calloc(1,sizeof(tNodo));
+printf("CERRADOS: \n");
+while(!esVacia(Auxiliar)){
+
+    ExtraerPrimero(Auxiliar,Sucesor,sizeof(tNodo));
+
+    dispEstado(Sucesor->estado);
+    Auxiliar = Auxiliar -> next;
+}
+
+printf("\nTERMINA.\n");
+
+
+}
---
+++ IA PUZZLE/puzleAlum.c
@@ -0,0 +1,153 @@
+/*******************************************/
+/* 		      PUZLE.C                      */
+/*						                   */
+/* Asignatura: Inteligencia Artificial     */
+/* Grado en Ingenieria Informatica - UCA   */
+/*******************************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "puzle.h"
+
+
+
+tEstado *crearEstado(int puzle[N][N])
+{
+   tEstado *estado = (tEstado *) malloc(sizeof(tEstado));
+   int i, j, ficha;
+
+   for (i=0;i<N;i++)
+      for (j=0;j<N;j++)
+      {
+         ficha=puzle[i][j];
+         estado->celdas[i][j]=ficha;
+         estado->fila[ficha]=i;
+         estado->col[ficha]=j;
+      }
+   return estado;
+}
+
+
+tEstado *estadoInicial()
+{
+   return crearEstado(puzle_inicial);
+}
+
+
+tEstado *estadoObjetivo()
+{
+   return crearEstado(puzle_final);
+}
+
+int coste(unsigned op, tEstado *estado)
+{
+   return 1;
+}
+
+/* VISUALIZACIÓN DE ESTADOS Y OPERADORES*/
+
+
+void dispEstado(tEstado *estado)
+{
+   int i,j;
+
+   for (i=0; i<N; i++)
+   {
+      for (j=0; j<N; j++)
+         printf("%d",estado->celdas[i][j]);
+      printf("\n");
+   }
+   printf("\n");
+}
+
+
+void dispOperador(unsigned op)
+{
+   switch(op)
+   {
+      case ARRIBA:    printf("Movimiento ARRIBA:\n"); break;
+      case ABAJO:     printf("Movimiento ABAJO:\n"); break;
+      case IZQUIERDA: printf("Movimiento IZQUIERDA:\n"); break;
+      case DERECHA:   printf("Movimiento DERECHA:\n"); break;
+   }
+}
+
+// FUNCIONES QUE SE HAN DE IMPLEMENTAR EN LA PRACTICA 1
+
+// Función auxiliar para comprobar si dos puzles tienen las fichas colocadas en el mismo orden en el tablero
+int iguales(tEstado *s, tEstado *t)  //
+{
+   int i = 0, j = 0, iguales = 1;
+
+  for(i=0;i<N && iguales != 0;i++){
+    for(j=0;j<N && iguales != 0;j++){
+        if(s->celdas[i][j] != t->celdas[i][j]){
+            iguales = 0;
+        }
+    }
+}
+
+
+  return iguales;
+}
+
+
+int testObjetivo(tEstado *estado)
+{
+    tEstado *objetivo;
+    objetivo = estadoObjetivo();
+
+return iguales(objetivo,estado);
+}
+
+
+int esValido(unsigned op, tEstado *estado)
+{
+    int Valido,i,j;
+     i =  estado->fila[0]; //filaDelHueco(*estado);
+     j =  estado->col[0];
+    switch(op){
+        case ARRIBA: Valido = ( i>0);break;
+        case ABAJO: Valido = (i<N-1);break;
+        case IZQUIERDA: Valido =(j>0);break;
+        case DERECHA: Valido = (j<N-1);break;
+        default: Valido = 0; break;
+    }
+return Valido;
+}
+
+
+tEstado *aplicaOperador(unsigned op, tEstado *estado)
+{
+      int i,j,aux,FilaNueva,ColNueva,v;
+     tEstado *nuevo= (tEstado *) malloc(sizeof(tEstado));
+    memcpy(nuevo, estado,sizeof(tEstado));  // Hace una copia del estado
+      i =  estado->fila[0]; //filaDelHueco(*estado);
+      j =  estado->col[0];
+     switch(op){
+        case ARRIBA: nuevo->fila[0] = nuevo->fila[0] - 1;break;
+        case ABAJO: nuevo->fila[0] = nuevo->fila[0] +1;break;
+        case IZQUIERDA: nuevo->col[0] = nuevo->col[0] -1;break;
+        case DERECHA: nuevo->col[0] = nuevo->col[0] +1;break;
+        default:break;
+    }
+
+    FilaNueva = nuevo->fila[0];    //Guardo la fila nueva originada en la que se almacenará el 0
+    ColNueva = nuevo->col[0];      //Guardo la columna nueva originada en la que se almacenará el 0
+    aux = nuevo->celdas[FilaNueva][ColNueva]; //Obtengo el número almacenado en la posición en la que vamos a intercambiarlo por el 0
+
+    nuevo->fila[aux] = i;                     //Guardo en la fila el numero a cambiar, por la posicion de la fila del 0
+    nuevo->col[aux] = j;                      //Guardo en la columna el numero a cambiar, por la posición de la columna del 0
+    nuevo->celdas[i][j] = aux;                //Guardo en la matriz con las posiciones de donde estaba antes el 0, con el numero intercambiado
+    nuevo->celdas[FilaNueva][ColNueva] = 0;   //Guardo en la matriz con la fila nueva y la columna nueva, la posicion del 0
+
+
+return nuevo;
+}
+
+
+
+
